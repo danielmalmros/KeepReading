d3b2525dd4425b61f419d01a553cb761969b10de
Daniel Malmros Christiansen <daniel@bymalmros.dk>
2017-01-07 11:56:51 +0100

    keep reading init

---
+++ js/keepreading.js
@@ -0,0 +1,21 @@
+$(() => {
+    let initKeepReading = () => {
+        keepReading();
+    };
+
+    let keepReading = () => {
+        let text = $('.keepreading').text();
+        let charsLength = text.length;
+        let wordsCount = text.trim().split(/\s+/g).length;
+
+        let readingTime = wordsCount / 200;
+
+        let readingTimeMinutes  = Math.round(readingTime)
+
+        console.log(readingTimeMinutes);
+
+        $('.keepreading__time').html(readingTimeMinutes)
+    };
+
+    initKeepReading();
+});
