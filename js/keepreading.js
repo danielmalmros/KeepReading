16e1c6a0cfaf1ba7ff2f1d44a14aaf151b08e612
Daniel Malmros Christiansen <daniel@bymalmros.dk>
2017-01-13 16:31:39 +0100

    added comments to the code

--- js/keepreading.js
+++ js/keepreading.js
@@ -6,22 +6,31 @@ $(() => {
 
     let keepReading = () => {
 
+        // Store text block class.
         let keepReading = $('.keepreading');
 
+        // Store the average reading time.
+        let averageReadingTime = 200;
+
+        // loop through each element that has the class '.keepreading'.
         $.each(keepReading, function(key, val) {
 
-            let text = $(this).text();
-            keepReadingTime = $(this).find('.keepreading__time');
-            let charsLength = text.length;
-            let wordsCount = text.trim().split(/\s+/g).length;
-            let readingTime = wordsCount / 200;
+            // Calculate each text block.
+            let keepReadingText = $(this).text();
+            let charsLength = keepReadingText.length;
+            let wordsCount = keepReadingText.trim().split(/\s+/g).length;
+            let readingTime = wordsCount / averageReadingTime;
             let readingTimeMinutes = Math.round(readingTime);
 
+            // Get the descendants of each element in the current set.
+            keepReadingTime = $(this).find('.keepreading__time');
+
+            // Add the calculated read time to DOM.
             $(keepReadingTime).html('Read time ' + (readingTimeMinutes) + ' minuts!');
 
         });
-
     };
 
     initKeepReading();
+
 });
